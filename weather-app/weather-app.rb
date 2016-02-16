74e80675e0ff48d402994d71441296e409d636e4
Dustin <singflan@gmail.com>
2016-02-16 09:51:07 -0300

    set up initial getting of location

--- weather-app/weather-app.rb
+++ weather-app/weather-app.rb
@@ -1,1 +1,16 @@
-require 'weather-underground'
+require 'yahoo_weatherman'
+
+puts "Please tell us your location:  "
+location = gets.chomp
+String weather
+def getWeather(location)
+	
+	client = Weatherman::Client.new
+
+	weather = client.lookup_by_location('#{location}').condition['text']
+
+	return weather
+end
+
+puts "The weather today in #{location} is #{weather}."
+
